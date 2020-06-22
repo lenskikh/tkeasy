d271611ba21d169eacc955c977fcf505bd21cd78
Mikhail Lenskikh <lenskikh@gmail.com>
2020-06-22 13:25:56 -0400

    Added the ability to erase text by mouse click, if it is present in the input field as a hint

--- tkeasy.py
+++ tkeasy.py
@@ -4,7 +4,7 @@ from tkinter import filedialog
 from tkinter import scrolledtext 
 
 root = tk.Tk()
-memory = {"filename":"", "checkbox 1":0, "convert to lower case":0,"multi":""}
+memory = {"filename":"", "event":0,"checkbox 1":0,"multi":"","focus":""}
 radioBox = tk.IntVar()
 radioBox.set(1)
 
@@ -23,6 +23,15 @@ def selection(*args):
     print("Radiobox = %s" % radioBox.get())
     print("Dropdown menu = %s" % memory["variable"].get())
     print("Dropdown menu = %s" % memory["variable2"].get())
+
+#clear entry if text inside field used as prompting
+def clear(event):
+    memoryForClear = str(root.focus_get())
+    if memoryForClear in memory["focus"]:
+        pass #entry field was cleared
+    else:
+        root.focus_get().delete(0, tk.END)
+        memory["focus"]+=memoryForClear   
 
 def alignment(sticky):
     if sticky == "right":
@@ -57,7 +66,7 @@ def labels(text,colortext,font,background,row,column,sticky):
 def entryfield(identifier,row,column):
     memory[identifier] = tk.Entry(root)
     memory[identifier].grid(row=row,column=column,sticky=tk.W,padx=10,pady=10)
-
+    
 def entryinsert(identifier,text,color):
     #memory["variable_entry"] = text
     #memory[identifier].insert(0,memory["variable_entry"])

1af8fd7c38c39fb3f7ebe988d71ea9fc46c33808
Mikhail Lenskikh <lenskikh@gmail.com>
2020-06-23 02:44:46 -0400

    added example #3 (dropdown list)

---
+++ examples/example3.py
@@ -0,0 +1,38 @@
+from tkeasy import *
+
+title("Example 3")
+
+memory["first"] = "First name"
+
+def info():
+    text1 = memory["entry 1"].get()
+    labels(text1,"white","16","grey",0,1,"center")
+
+    text2 = memory["entry 2"].get()
+    labels(text2,"white","16","grey",1,1,"center")
+
+    gender = memory["variable"].get()
+    labels(gender,"white","16","grey",2,1,"center")
+
+    age = memory["variable2"].get()
+    labels(age,"white","16","grey",3,1,"center")     
+
+entryfield("entry 1",0,0)
+entryinsert("entry 1",memory["first"],"black")
+
+entryfield("entry 2",1,0)
+entryinsert("entry 2","Second name","black")
+
+choices = ["=","Female","Male"]
+dropdownlist(choices,"variable","Gender",2,0)
+
+age = ["01-06","7-14","15-23","24-31","31-40","41-55","56-70","71-85","86-105"]
+dropdownlist(age,"variable2","Age",3,0)
+
+buttons("Get info",info,4,1,"right")
+
+#clear text when click in entry field
+root.bind("<Button-1>", clear)
+root.bind("<Key>", key)
+
+root.mainloop()

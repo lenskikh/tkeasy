201e0d7a5e0f09dc47422ef6f9c4b7a2cfcf7e01
Mikhail Lenskikh <lenskikh@gmail.com>
2020-06-23 02:50:42 -0400

    changed names of variables in dropdown list

--- examples/example3.py
+++ examples/example3.py
@@ -11,10 +11,10 @@ def info():
     text2 = memory["entry 2"].get()
     labels(text2,"white","16","grey",1,1,"center")
 
-    gender = memory["variable"].get()
+    gender = memory["gender var"].get()
     labels(gender,"white","16","grey",2,1,"center")
 
-    age = memory["variable2"].get()
+    age = memory["age var"].get()
     labels(age,"white","16","grey",3,1,"center")     
 
 entryfield("entry 1",0,0)
@@ -24,10 +24,10 @@ entryfield("entry 2",1,0)
 entryinsert("entry 2","Second name","black")
 
 choices = ["=","Female","Male"]
-dropdownlist(choices,"variable","Gender",2,0)
+dropdownlist(choices,"gender var","Gender",2,0)
 
 age = ["01-06","7-14","15-23","24-31","31-40","41-55","56-70","71-85","86-105"]
-dropdownlist(age,"variable2","Age",3,0)
+dropdownlist(age,"age var","Age",3,0)
 
 buttons("Get info",info,4,1,"right")
 

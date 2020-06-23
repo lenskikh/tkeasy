5ed9f3d29e0d3de6642d6c9d24f28f267f4728d5
Mikhail Lenskikh <lenskikh@gmail.com>
2020-06-22 22:23:23 -0400

    fixed case when using TAB key

--- tkeasy.py
+++ tkeasy.py
@@ -30,12 +30,19 @@ def clear(event):
         memoryForClear = str(root.focus_get())
         if memoryForClear in memory["focus"]:
             pass #entry field was cleared
+        elif "TAB" in memory["focus"]:
+            pass
         else:
             root.focus_get().delete(0, tk.END)
             memory["focus"]+=memoryForClear
     except:
         pass
-
+    
+def key(event):
+    char = str(event.char)
+    if char == '\t':
+        memory["focus"]+="TAB"
+    
 def alignment(sticky):
     if sticky == "right":
         sticky = tk.E
@@ -53,7 +60,7 @@ def dialog():
 def buttons(text,command,row,column,sticky):
     sticky = alignment(sticky) 
     tk.Button(root, text=text,command=command).grid(
-        row=row,column=column,sticky=sticky,padx=10,pady=10)
+        row=row,column=column,sticky=sticky,padx=2,pady=2)
 
 def labels(text,colortext,font,background,row,column,sticky):
     if font == "":
@@ -64,40 +71,38 @@ def labels(text,colortext,font,background,row,column,sticky):
         background = "white"
     sticky = alignment(sticky)      
     tk.Label(root, text=text,fg=colortext,font=font,bg=background).grid(
-        row=row,column=column,sticky=sticky,padx=10,pady=10)
+        row=row,column=column,sticky=sticky,padx=2,pady=2)
 
 def entryfield(identifier,row,column):
     memory[identifier] = tk.Entry(root)
-    memory[identifier].grid(row=row,column=column,sticky=tk.W,padx=10,pady=10)
+    memory[identifier].grid(row=row,column=column,sticky=tk.W,padx=2,pady=2)
     
 def entryinsert(identifier,text,color):
-    #memory["variable_entry"] = text
-    #memory[identifier].insert(0,memory["variable_entry"])
     memory[identifier].insert(0,text)
-    memory[identifier].config(fg=color)        
+    memory[identifier].config(fg=color)
 
 def checkbox(identifier,text,row,column):
     memory[identifier] = tk.IntVar()
     memory[text] = tk.Checkbutton(root,text=text,variable=memory[identifier])
-    memory[text].grid(row=row,column=column,sticky=tk.W,padx=10,pady=10)
+    memory[text].grid(row=row,column=column,sticky=tk.W,padx=2,pady=2)
 
 def radiobox(text,value,row,column):
     radiob = tk.Radiobutton(root, text=text, variable=radioBox,command=selection, value=value)
-    radiob.grid(row=row,column=column,sticky=tk.W,padx=10,pady=10)
+    radiob.grid(row=row,column=column,sticky=tk.W,padx=2,pady=2)
 
 def popup(choices,variable,default,row,column):
     memory[variable] = tk.StringVar(root)
     popupmenu = tk.OptionMenu(root, memory[variable], command=selection, *choices)
     memory[variable].set(default) # default value
-    popupmenu.grid(row=row,column=column,sticky=tk.W,padx=10,pady=10)
+    popupmenu.grid(row=row,column=column,sticky=tk.W,padx=2,pady=2)
 
 def textarea(identifier,row,column):
     memory[identifier] = tk.Text(root,height=10, width=30,background="grey95")
-    memory[identifier].grid(row=row,column=column,sticky=tk.W,padx=10,pady=10)
+    memory[identifier].grid(row=row,column=column,sticky=tk.W,padx=2,pady=2)
 
 def textareascroll(identifier,row,column):
     memory[identifier] = scrolledtext.ScrolledText(root,wrap = tk.WORD,height=10, width=30,background="grey95")
-    memory[identifier].grid(row=row,column=column,sticky=tk.W,padx=10,pady=10)
+    memory[identifier].grid(row=row,column=column,sticky=tk.W,padx=2,pady=2)
 
 def instertextarea(identifier,text,color):    
     memory[identifier].insert(1.0,text)

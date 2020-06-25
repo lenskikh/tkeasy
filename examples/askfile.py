e1cc0ae064dbfb788937d0caa71576d0ac0dd230
Mikhail Lenskikh <lenskikh@gmail.com>
2020-06-25 18:20:07 -0400

    added example - ask file

---
+++ examples/askfile.py
@@ -0,0 +1,15 @@
+#ask folder - gui widget
+from tkeasy import *
+
+title("Ask folder")
+
+def readname():
+    print (memory["filename"])
+
+labels("ASK FILE","white","16","BLUE",0,0,"center")
+
+buttons(text='FILE BROWSER',command=selectfile,row=2,column=0,sticky="center")
+
+buttons("Print Name Of File",command=readname,row=3,column=0,sticky="center")
+
+root.mainloop()

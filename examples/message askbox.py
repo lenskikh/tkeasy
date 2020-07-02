31480efa89899598d9443f49f57da142e7d48e16
Mikhail Lenskikh <lenskikh@gmail.com>
2020-07-02 01:40:05 -0400

    added 'ask question' example

---
+++ examples/message askbox.py
@@ -0,0 +1,11 @@
+from tkeasy import *
+
+title("Ask message box")
+
+def showinfo():
+    print(memory["ask"])
+    
+msgboxask("ask","Message box","A system can't find file. Would you like created it?")
+buttons("get info",showinfo,0,0,"center")
+
+root.mainloop()

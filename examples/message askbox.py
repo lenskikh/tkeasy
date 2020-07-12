d33844b1155cdf3eef987d103da6041758e6f487
Mikhail Lenskikh <lenskikh@gmail.com>
2020-07-11 23:09:59 -0400

    fixed title

--- examples/message askbox.py
+++ examples/message askbox.py
@@ -1,11 +1,9 @@
 from tkeasy import *
 
-title("Ask message box","")
+title_size("Ask message box","")
 
 def showinfo():
     print(memory["ask"])
     
 msgboxask("ask","Message box","A system can't find file. Would you like created it?")
 buttons("get info",showinfo,0,0,"center")
-
-root.mainloop()

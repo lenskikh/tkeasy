a697fd6ffcc3deec64925832e0e7835e058ce09f
Mikhail Lenskikh <lenskikh@gmail.com>
2020-07-24 00:01:32 -0400

    fixed example

--- examples/askfolder.py
+++ examples/askfolder.py
@@ -1,15 +1,15 @@
 #ask folder - gui widget
 from tkeasy import *
 
-title_size("Ask folder","")
+#root is default var of Tk
+root.title("Three entries")
+root.geometry("150x80")
 
 def readname():
     print (memory["filename"])
 
-labels("ASK FOLDER","white","16","green",0,0,"center")
+labels(text="ASK FOLDER",background="green",row=0,column=0)
 
 buttons(text='FOLDER BROWSER',command=selectfolder,row=2,column=0,sticky="center")
 
-buttons("Print Name Of Folder",command=readname,row=3,column=0,sticky="center")
-
-root.mainloop()
+buttons(text="Print Name Of Folder",command=readname,row=3,column=0,sticky="center")

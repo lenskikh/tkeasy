b039347c32f04bb5fd8871344914ff42cc894411
Mikhail Lenskikh <lenskikh@gmail.com>
2020-07-26 23:59:33 -0400

    changed example

--- examples/radiobox_example.py
+++ examples/radiobox_example.py
@@ -6,4 +6,4 @@ def show_info():
 radiobox(default="Melon",text="Apple",row=0,column=0)
 radiobox(text="Melon",row=1,column=0,value="weight = 2kg")
 radiobox(text="Lemon",row=2,column=0)
-buttons(text="Show Info",command=show_info,row=3,column=0)
+button(text="Show Info",command=show_info,row=3,column=0)

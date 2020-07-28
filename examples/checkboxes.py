2750f4312e2e939ca60cb4e8326a55eb32de3bde
Mikhail Lenskikh <lenskikh@gmail.com>
2020-07-27 23:21:47 -0400

    fixed example

--- examples/checkboxes.py
+++ examples/checkboxes.py
@@ -5,7 +5,7 @@ def info():
     print(memory["checkbox 2"].get())
     print(memory["entry2"].get())
 
-labels(text="What kind of fruit do you like?",row=0,column=0)    
+label(text="What kind of fruit do you like?",row=0,column=0)    
 checkbox("checkbox 1", text="Apple",row=1,column=0,sticky = "left")
 checkbox("checkbox 2", text="Banan",row=2,column=0,sticky = "left")
 entryfield("entry2",row=3,column=0)

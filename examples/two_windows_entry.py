5396964ba49489df02cfd5cf96fbe01e8ed77946
Mikhail Lenskikh <lenskikh@gmail.com>
2020-07-29 15:18:51 -0400

    Added new example

---
+++ examples/two_windows_entry.py
@@ -0,0 +1,27 @@
+from tkeasy import *
+
+def info():
+    print(memory["entry 1"].get())
+
+def info2():
+    print(memory["entry 2"].get())
+
+#The first window
+title(window="main",text="First window")
+geometry(window="main",size="196x100")
+label(window="main",text="Provide your info",
+      background="grey90",row=0,column=0)
+entryfield(window="main",name="entry 1",
+           row=1,column=0)
+button(window="main",text="Get Info",
+       command=info,row=2,column=0)
+
+#The second window
+title(window="second",text="Second window")
+label(window="second",text="Provide your info",
+      colortext="white",background="green",
+      row=0,column=0)
+entryfield(window="second",name="entry 2",
+           row=1,column=0)
+button(window="second",text="Get Info",
+       command=info2,row=2,column=0)

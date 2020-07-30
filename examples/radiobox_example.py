36b85cc31d5ffbeb52252f54c98df43147b80f4c
Mikhail Lenskikh <lenskikh@gmail.com>
2020-07-30 02:12:12 -0400

    added example of radiobox

---
+++ examples/radiobox_example.py
@@ -0,0 +1,13 @@
+from tkeasy import *
+
+def show_info():
+    choice = memory["radioBox"].get()
+    if choice == "None":
+        msgboxwarning("warning","Choose something, please")
+    else:
+        msgbox("Your choice",f'Your choice is {choice}')
+
+radiobox(window="main",text="Apple",row=0,column=0)
+radiobox(window="main",text="Melon",row=1,column=0,value="weight = 2kg")
+radiobox(window="main",text="Lemon",row=2,column=0)
+button(window="main",text="Show Info",command=show_info,row=3,column=0)

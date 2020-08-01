1a9131a6139ac38a7b4fb981ead9162f6a44c5ad
Mikhail Lenskikh <lenskikh@gmail.com>
2020-08-01 02:21:46 -0400

    example updated

--- examples/askfile.py
+++ examples/askfile.py
@@ -4,13 +4,13 @@ from tkeasy import *
 title(window="main",text="Ask file")
 
 def readname():
-    print (getinfo("file"))
+    print (get_info("file"))
 
 label(window="main",text="ASK FILE",colortext="white",
       background="BLUE",row=0,column=0)
 
 button(window="main",text="FILE BROWSER",
-       command=selectfile,row=1,column=0)
+       command=select_file,row=1,column=0)
 
 button(window="main",text="Print Name Of File",
        command=readname,row=2,column=0)

ef8e94db4e7e4cc12b339bbd20f8c5d4a00fc2f3
Mikhail Lenskikh <lenskikh@gmail.com>
2020-08-01 02:17:39 -0400

    changed example

--- examples/2droplists.py
+++ examples/2droplists.py
@@ -6,19 +6,19 @@ def info():
     title(window="second",text="Info")
     label(window="second",text="Gender",
       background="green",colortext="white",row=0,column=0)
-    label(window="second",text=getinfo("gender var"),
+    label(window="second",text=get_info("gender var"),
       background="grey90",row=0,column=1)
     label(window="second",text="Age",
       background="green",colortext="white",row=1,column=0)    
-    label(window="second",text=getinfo("age var"),
+    label(window="second",text=get_info("age var"),
       background="grey90",row=1,column=1)    
 
 choices = ["=","Female","Male"]
-dropdownlist(window="main",variable="gender var",choices=choices,
+dropdown_list(window="main",variable="gender var",choices=choices,
              default="Gender",row=0,column=0)
 
 age = ["01-06","7-14","15-23","24-31","31-40","41-55","56-70","71-85","86-105"]
-dropdownlist(window="main",variable="age var",choices=age,
+dropdown_list(window="main",variable="age var",choices=age,
              default="Age",row=1,column=0)
 
 button(window="main",text="Get Info",

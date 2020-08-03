4973056653a352725690dee74b2dd0695470bdd4
Mikhail Lenskikh <lenskikh@gmail.com>
2020-08-03 18:36:07 -0400

    added example

---
+++ open_links.py
@@ -0,0 +1,14 @@
+from tkeasy import *
+
+label(window="main",text="Python official website",
+      colortext="blue",row=0,column=0,
+      link="https://www.python.org/")
+
+label(window="main",text="Google official website",
+      colortext="blue",row=1,column=0,
+      link="https://www.google.com")
+
+label(window="main",text="Gmail website",
+      colortext="blue",row=2,column=0,
+      link="https://www.gmail.com")
+

ee41033cc5d44690e084918d2daeadc0f0644cf0
Mikhail Lenskikh <lenskikh@gmail.com>
2020-08-03 22:03:18 -0400

    'minor'

--- git_example.py
+++ git_example.py
@@ -1,14 +1,15 @@
+#test
 import subprocess
 
 process = subprocess.Popen(["git", "status"], stdout=subprocess.PIPE)
 output = process.communicate()[0]
 print(output)
 
-process = subprocess.Popen(["git", "add ."], stdout=subprocess.PIPE)
+process = subprocess.Popen(["git", "add", "."], stdout=subprocess.PIPE)
 output = process.communicate()[0]
 print(output)
 
-process = subprocess.Popen(["git", "commit -m 'minor'"], stdout=subprocess.PIPE)
+process = subprocess.Popen(["git", "commit","-m 'minor'"], stdout=subprocess.PIPE)
 output = process.communicate()[0]
 print(output)
 

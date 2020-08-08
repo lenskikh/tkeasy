2dc37904b39fa79373637bf9d2a19d708fc22b1e
Mikhail Lenskikh <lenskikh@gmail.com>
2020-08-08 19:31:31 -0400

    changed example

--- example_askfolder.py
+++ example_askfolder.py
@@ -2,19 +2,20 @@
 from tkeasy import *
 
 #root is default var of Tk
-title(window="main",text="Ask Folder")
-geometry(window="main",size="150x80")
+title(text="Ask Folder")
+geometry(size="150x80")
 
 def readname():
-    print(getinfo("folder"))
+    print(get_info("folder"))
 
-label(window="main",text="ASK FOLDER",colortext="white",
+label(text="ASK FOLDER",colortext="white",
       background="green",row=0,column=0)
 
-button(window="main", text='FOLDER BROWSER',
-       command=selectfolder,row=1,column=0,
-       sticky="center")
+button(text='FOLDER BROWSER',command=select_folder,
+       row=1,column=0,sticky="center")
 
-button(window="main",text="Print Name Of Folder",
+button(text="Print Name Of Folder",
        command=readname,row=3,column=0,
        sticky="center")
+
+app_loop()

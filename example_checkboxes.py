411954cdf06d8d9f45c249d84d9a4099a4923c8e
Mikhail Lenskikh <lenskikh@gmail.com>
2020-08-08 19:33:57 -0400

    changed example

--- example_checkboxes.py
+++ example_checkboxes.py
@@ -4,13 +4,15 @@ def info():
     print(get_info("checkbox 1"))
     print(get_info("checkbox 2"))
 
-label(window="main",text="What a fruit do you like?",row=0,column=0)
+label(text="What a fruit do you like?",row=0,column=0)
 
-checkbox(window="main",name="checkbox 1", text="Apple",
+checkbox(name="checkbox 1", text="Apple",
          row=1,column=0,sticky = "left")
 
-checkbox(window="main",name="checkbox 2", text="Banan",
+checkbox(name="checkbox 2", text="Banan",
          row=2,column=0,sticky = "left")
 
-button(window="main",text="show info",
+button(text="show info",
        command=info,row=3,column=0)
+
+app_loop()

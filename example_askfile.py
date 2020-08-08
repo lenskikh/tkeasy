e0a802ceb05f61215980e203e19edd2d1101b786
Mikhail Lenskikh <lenskikh@gmail.com>
2020-08-08 19:26:10 -0400

    changed example

--- example_askfile.py
+++ example_askfile.py
@@ -1,18 +1,18 @@
 #ask folder - gui widget
 from tkeasy import *
 
-title(window="main",text="Ask file")
+title(text="Ask file")
 
 def readname():
     print (get_info("file"))
 
-label(window="main",text="ASK FILE",colortext="white",
+label(text="ASK FILE",colortext="white",
       background="BLUE",row=0,column=0)
 
-button(window="main",text="FILE BROWSER",
+button(text="FILE BROWSER",
        command=select_file,row=1,column=0)
 
-button(window="main",text="Print Name Of File",
+button(text="Print Name Of File",
        command=readname,row=2,column=0)
 
-
+app_loop()

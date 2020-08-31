a1a2df91714e9f2a6e8817b951e52f49c3224da6
Mikhail Lenskikh <lenskikh@gmail.com>
2020-08-30 23:40:29 -0400

    Slider-example was example

--- example_sliders.py
+++ example_sliders.py
@@ -8,12 +8,12 @@ title("Sliders")
 
 label(text="Velocity",row=0,column=0)
 #horizontal scale by default    
-scale(name="scale 1",row=0,column=1)
+slider(name="scale 1",row=0,column=1)
 separator(column_length=2,pady=5)
 
 label(text="Chrominance",row=2,column=0)
 #horizontal scale by default    
-scale(name="scale 2",row=2,column=1)
+slider(name="scale 2",row=2,column=1)
 button("get slider values",info,3,1)
 
 app_loop()

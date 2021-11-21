2965c123559c25fa05347d124438a3bf5a9ed198
lenskikh <lenskikh@gmail.com>
2021-11-20 22:49:50 -0500

    Example of calc was updated

--- examples/example_calculator.py
+++ examples/example_calculator.py
@@ -1,18 +1,51 @@
 from tkeasy import *
 
-label(frame="1",x=10,y=10,text="" ,background="#7cc5ba",font=14,row=0,column=0,padx=0,pady=0)
+title("Calc")
+config(size="200x230",background="white",icon="icon.ico")
 
-frame3 = Frame(advanced(),highlightbackground="#7cc5ba",highlightthickness=1,width=110, height=101)
-frame3.place(x=10,y=50)
+the_first_frame = "x=27,y=10,border_thickness=1,border_color=#7cc5ba,padx=2,pady=5"
+entry(frame=the_first_frame,name="entry 1",width=23,row=1,column=0)
 
-button(frame="2",x=15,y=55,text="  7  ",command=False,row=0,column=0)
-button(frame="2",x=15,y=55,text="  8  ",command=False,row=0,column=1)
-button(frame="2",x=15,y=55,text="  9  ",command=False,row=0,column=2)
+the_second_frame="x=27,y=50,border_thickness=1,border_color=#7cc5ba,padx=5,pady=6"
 
-button(frame="2",x=15,y=55,text="  4  ",command=False,row=1,column=0)
-button(frame="2",x=15,y=55,text="  5  ",command=False,row=1,column=1)
-button(frame="2",x=15,y=55,text="  6  ",command=False,row=1,column=2)
+screen = ""
 
-button(frame="2",x=15,y=55,text="  1  ",command=False,row=2,column=0)
-button(frame="2",x=15,y=55,text="  2  ",command=False,row=2,column=1)
-button(frame="2",x=15,y=55,text="  3  ",command=False,row=2,column=2)
+def update(number):
+    global screen
+    screen+=number
+    entry_clear("entry 1")
+    insert_text(name="entry 1",text=screen,color="black")
+
+def result():
+    entry_clear("entry 1")
+    total = eval(screen)
+    insert_text(name="entry 1",text=total,color="black")
+
+def AC():
+    global screen
+    screen = ""
+    entry_clear("entry 1")
+    insert_text(name="entry 1",text=screen,color="black")
+
+button(frame=the_second_frame,text=" AC ",command=AC,row=0,column=0)
+
+button(frame=the_second_frame,text="  7  ",command=lambda:update("7"),row=1,column=0)
+button(frame=the_second_frame,text="  8  ",command=lambda:update("8"),row=1,column=1)
+button(frame=the_second_frame,text="  9  ",command=lambda:update("9"),row=1,column=2)
+
+button(frame=the_second_frame,text="  4  ",command=lambda:update("4"),row=2,column=0)
+button(frame=the_second_frame,text="  5  ",command=lambda:update("5"),row=2,column=1)
+button(frame=the_second_frame,text="  6  ",command=lambda:update("6"),row=2,column=2)
+
+button(frame=the_second_frame,text="  1  ",command=lambda:update("1"),row=3,column=0)
+button(frame=the_second_frame,text="  2  ",command=lambda:update("2"),row=3,column=1)
+button(frame=the_second_frame,text="  3  ",command=lambda:update("3"),row=3,column=2)
+
+button(frame=the_second_frame,text="  0  ",command=lambda:update("0"),row=4,column=0)
+button(frame=the_second_frame,text="  .  ",command=lambda:update("."),row=4,column=1)
+button(frame=the_second_frame,text="  =  ",command=result,row=4,column=2)
+
+button(frame=the_second_frame,text="  /  ",command=lambda:update("/"),row=1,column=4)
+button(frame=the_second_frame,text="  x  ",command=lambda:update("*"),row=2,column=4)
+button(frame=the_second_frame,text="  -  ",command=lambda:update("-"),row=3,column=4)
+button(frame=the_second_frame,text="  +  ",command=lambda:update("+"),row=4,column=4)
